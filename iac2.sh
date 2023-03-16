d50edffe61c321899badeba68412aa0055a77967
GabrielLim4 <gabriellimasjc@gmail.com>
2023-03-15 21:09:07 -0300

    First commit

---
+++ iac2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Atualizando servidor..."
+sudo apt update
+sudo apt upgrade -y
+
+
+echo "Instalando o Apache2 e Unzip..."
+sudo apt install apache2 -y
+sudo apt install unzip -y
+
+
+echo "Baixando e descompactando aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+
+echo "Copiando arquivos para o diretório padrão do Apache..."
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+echo "Procedimento finalizado."
